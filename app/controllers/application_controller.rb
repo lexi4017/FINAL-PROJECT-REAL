f0d402cfa85de6c3355bdf5e16aad911991d5981
lexi4017 <41583800+lexi4017@users.noreply.github.com>
2018-08-01 16:45:06 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,15 @@ class ApplicationController < Sinatra::Base
   
   post '/' do
     scoring(params[:question1])
+    scoring(params[:question2])
+    scoring(params[:question3])
+    scoring(params[:question4])
+    scoring(params[:question5])
+    scoring(params[:question6])
+    scoring(params[:question7])
+    scoring(params[:question8])
+    scoring(params[:question9])
+    @@the_real_result = request
       
     
       erb :results
